c0b2b0cb9dbc89f28c6dd9c351b1fc3b188cc561
Simon La Grange <sla-gran@fu-r5-p3.s19.be>
2025-12-10 16:13:07 +0100

    ex7 corrige

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def ft_seed_inventory(seed_type: str, quantity:    int, unit:  str) -> None:
-    print(seed_type, "seeds:", quantity, unit, "available")
+    if (unit == "packets"):
+        print(f"{seed_type.capitalize} seeds: {quantity} packets available")
+    elif (unit == "gram"):
+        print(f"{seed_type.capitalize} seeds: {quantity} grams total")
+    elif (unit == "area"):
+        print(f"{seed_type.capitalize} seeds: covers {quantity} square meters")
+
