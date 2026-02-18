8a06bfdb9a2bd8027c133d7cad6e59f46267200d
Simon La Grange <sla-gran@fu-r4-p8.s19.be>
2026-02-18 18:09:54 +0100

    correction count_harvest_recursive

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -4,8 +4,13 @@
 def ft_count_harvest_recursive(day: int = -1) -> None:
     if day == -1:
         ft_count_harvest_recursive(int(input("Days until harvest: ")))
-        print("Harvest time!")
+        if day >= 0:
+            print("Harvest time!")
+            return
+    if day < -1:
+        print(f"Harvest passed {-day} days ago !!!")
         return
     if day > 1:
         ft_count_harvest_recursive(day - 1)
-    print(f"Day {day}")
+    if day == 0:
+        print(f"Day {day}")
