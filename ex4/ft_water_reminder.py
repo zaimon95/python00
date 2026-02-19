c648f742e91d050316d721736fb57cff0ae2c426
Simon La Grange <sla-gran@fu-r4-p8.s19.be>
2026-02-19 12:03:05 +0100

    modification ex4

--- ex4/ft_water_reminder.py
+++ ex4/ft_water_reminder.py
@@ -3,7 +3,7 @@
 
 def ft_water_reminder() -> None:
     water = int(input("Days since last watering: "))
-    if water >sss 2:
+    if water > 2:
         print("Water the plants!")
     else:
         print("Plants are fine")
