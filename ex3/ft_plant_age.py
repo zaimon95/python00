612453e58ea272cd71d27ea4deea82812644f567
Simon La Grange <sla-gran@fu-r4-p8.s19.be>
2026-02-19 10:19:52 +0100

    .gitignore

--- ex3/ft_plant_age.py
+++ ex3/ft_plant_age.py
@@ -3,7 +3,7 @@
 
 def ft_plant_age() -> None:
     age = int(input("Enter plant age in days: "))
-    if (age >= 60):
+    if (age > 60):
         print("Plant is ready to harvest!")
     else:
         print("Plant needs more time to grow.")
--- ex4/ft_water_reminder.py
+++ ex4/ft_water_reminder.py
@@ -3,7 +3,7 @@
 
 def ft_water_reminder() -> None:
     water = int(input("Days since last watering: "))
-    if water >= 2:
+    if water >sss 2:
         print("Water the plants!")
     else:
         print("Plants are fine")
